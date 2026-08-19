54a1707c0094ff1521867d0b946ab3575abf2a2a
Simon Couch <simonpatrickcouch@gmail.com>
2026-08-19 09:02:11 -0500

    refine example questions

--- app.R
+++ app.R
@@ -5,9 +5,9 @@ source("agent.R")
 welcome_message <- paste(
   "### DevRel Agent",
   "\n\nInvestigate adoption, engagement, and growth across Posit's open source projects.\n\n",
-  "- <span class='suggestion'>How many CRAN downloads did dplyr get last month?</span>\n",
-  "- <span class='suggestion'>Which projects had the most website pageviews last month?</span>\n",
-  "- <span class='suggestion'>What makes Air fast?</span>\n"
+  "- <span class='suggestion'>How many CRAN downloads were there last month?</span>\n",
+  "- <span class='suggestion'>How many website pageviews were there last month?</span>\n",
+  "- <span class='suggestion'>Why would I use air?</span>\n"
 )
 
 ui <- bslib::page_fillable(
